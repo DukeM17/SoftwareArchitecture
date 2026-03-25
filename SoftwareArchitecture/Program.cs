228bfcab8b42a190b5ecf9b2522f5c53c3165bdd
m.duke401 <m.duke401@gmail.com>
2026-03-25 21:59:27 +0000

    Creating logger using Singleton pattern

--- SoftwareArchitecture/Program.cs
+++ SoftwareArchitecture/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SoftwareArchitecture.Singleton;
+
+Logger.Instance.Log("test message");
+
+Logger.Instance.Log("test message2");
---
+++ SoftwareArchitecture/Singleton/Logger.cs
@@ -0,0 +1,28 @@
+﻿namespace SoftwareArchitecture.Singleton
+{
+    public class Logger
+    {
+        private Logger() { }
+
+        private static Logger _instance;
+        private static readonly object _lock = new();
+
+        public static Logger Instance
+        {
+            get 
+            {           
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        _instance ??= new();
+                    }                
+                }
+                return _instance;
+            }
+        }
+
+        public void Log(string message) =>
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+    }
+}
