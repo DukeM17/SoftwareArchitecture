f043d390ab400bd0ddd5c6b036d4a040ba8ebd14
m.duke401 <m.duke401@gmail.com>
2026-04-13 21:57:30 +0100

    object pool pattern

---
+++ SoftwareArchitecture/Design Patterns/Creational Patterns/ObjectPool/DatabaseConnection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Concurrent;
+
+namespace SoftwareArchitecture.Design_Patterns.Creational_Patterns.ObjectPool
+{
+    public class DatabaseConnection
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public bool IsOpen { get; private set; }
+
+        public void Open() { IsOpen = true; Console.WriteLine($"Connection {Id} is open"); }
+        public void Close() { IsOpen = false; Console.WriteLine($"Connection {Id} is closed"); }
+        public void Reset() { /* clear state for reuse */ }
+        public void ExecuteQuery(string sql) => Console.WriteLine($"[{Id}] Running: {sql}");
+    }
+
+    public class ConnectionPool
+    {
+        private readonly ConcurrentQueue<DatabaseConnection> _available = new(); // a list of database connections that are available
+        private readonly int _maxSize = 10;
+        private int _totalCreated;
+
+        public ConnectionPool(int maxSize)
+        {  _maxSize = maxSize; }
+
+        public DatabaseConnection Rent()
+        {
+            if (_available.TryDequeue(out var connection))
+            {
+                Console.WriteLine($"Reusing connection {connection.Id}");
+                return connection;
+            }
+
+            if (_totalCreated > _maxSize)
+                throw new InvalidOperationException("Connection pool exhausted - no connection was available");
+
+            // otherwise, make a new connection
+            var newConnection = new DatabaseConnection();
+            newConnection.Open();
+            Interlocked.Increment(ref _totalCreated);
+            return newConnection;
+        }
+
+        public void Return(DatabaseConnection connection)
+        {
+            connection.Reset();
+            _available.Enqueue(connection);
+            Console.WriteLine($"Connection {connection.Id} returned to pool");
+        }
+    }
+}
--- SoftwareArchitecture/Program.cs
+++ SoftwareArchitecture/Program.cs
@@ -1,13 +1,16 @@
-﻿using SoftwareArchitecture.Design_Patterns.Creational_Patterns.Builder;
+﻿using SoftwareArchitecture.Design_Patterns.Creational_Patterns.ObjectPool;
 
-var order = new OrderBuilder()
-    .ForItem("pizza")
-    .ForQuantity(0)
-    .ForSize("large")
-    .ForCrust("stuffed")
-    .ForExtras("Dough balls")
-    .ForDeliveryTime("asap")
-    .ForContactlessDelivery(true)
-    .Build();
+var pool = new ConnectionPool(maxSize: 3);
 
-Console.WriteLine(order.ToString());
+var connection = pool.Rent();
+connection.ExecuteQuery("SELECT * FROM mydatabase");
+pool.Return(connection);
+
+var connection2 = pool.Rent(); // The same connection is reused here
+connection2.ExecuteQuery("SELECT * FROM mydatabase2");
+
+var connection3 = pool.Rent(); // A new connection is made here
+connection2.ExecuteQuery("SELECT * FROM mydatabase3");
+
+pool.Return(connection2);
+pool.Return(connection3);
